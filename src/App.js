84bc897480653a26906a7f044a28a69968a48fa3
ayusha2806 <anandayush2806@gmail.com>
2023-12-04 17:33:06 +0530

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 294,
+      date: new Date(2023, 2, 19)
+    },
+    {
+      id: 'e2',
+      title: 'New Desk',
+      amount: 454,
+      date: new Date(2023, 5, 12)
+    },
+    {
+      id: 'e3',
+      title: 'New TV',
+      amount: 724,
+      date: new Date(2023, 6, 9)
+    },
+    {
+      id: 'e4',
+      title: 'Car',
+      amount: 9876,
+      date: new Date(2023, 2, 24)
+    },
+    {
+      id: 'e5',
+      title: 'New House',
+      amount: 34600,
+      date: new Date(2023, 4, 26)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
-function ExpenseItem() {
-  return (
-    <div>
-      <div>
-        <h2>Expense Items</h2>
-      </div>
-      <div>
-        <div> Food Rs 10</div>
-        <div> Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
-      </div>
-    </div>
-  );
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+
+                <div className='expense-item__price'> ${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
-export default ExpenseItem;
+export default ExpenseItem; 
