ca32928ca61895da36fe8133d73a00eac0a5fcff
ayusha2806 <anandayush2806@gmail.com>
2023-12-04 15:05:17 +0530

    let's Start Coding

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Expense Items</h2>
+      </div>
+      <div>
+        <div> Food Rs 10</div>
+        <div> Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
