2158a2ebe15591b7b0179fcf0ddf755b1f454c58
ayusha2806 <anandayush2806@gmail.com>
2023-12-06 21:10:45 +0530

    Making the expense form input visible on screen

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses'
+import Expenses from './components/Expenses/Expenses';
 
-const App=()=> {
-  const expenses = [
+const App = () => {
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
-      title: 'Car Insurance',
-      amount: 294,
-      date: new Date(2023, 2, 19)
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'New Desk',
-      amount: 454,
-      date: new Date(2023, 5, 12)
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'New TV',
-      amount: 724,
-      date: new Date(2023, 6, 9)
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'Car',
-      amount: 9876,
-      date: new Date(2023, 2, 24)
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
-    {
-      id: 'e5',
-      title: 'New House',
-      amount: 34600,
-      date: new Date(2023, 4, 26)
-    }
-  ]
+  ]);
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2',{},"Let's get Started!"),
-  //   React.createElement(Expenses,{items:expenses})
-  // )
+  const addExpenseHandler = (expense) => {
+    // Update the expenses state with the new expense
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
   return (
     <div>
-    <NewExpense/>
-    <Expenses items={expenses}/>
-  </div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
